663c078e19626cbb32921212a4b393e0f04256ef
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-09-21 20:29:19 -0700

    solved: 36

---
+++ 36_Valid-Sudoku.py
@@ -0,0 +1,37 @@
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+
+                if num == ".":
+                    continue
+
+                if num in rows[i]:
+                    return False
+                rows[i].add(num)
+
+                if num in cols[j]:
+                    return False
+                cols[j].add(num)
+
+                box_ind = i // 3 * 3 + j // 3
+                if num in boxes[box_ind]:
+                    return False
+                boxes[box_ind].add(num)
+
+        return True
