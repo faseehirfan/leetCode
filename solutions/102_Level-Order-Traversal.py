c41d7022422879d0a2b60d59e8f83a1242c10b7d
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-11-18 20:19:41 -0800

    solved: 102

---
+++ solutions/102_Level-Order-Traversal.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+### DFS SOLUTION
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        def dfs(root, level):
+            if not root: return None
+            if level == len(res):
+                res.append([])
+            res[level].append(root.val)
+            dfs(root.left, level + 1)
+            dfs(root.right, level + 1)
+
+        dfs(root, 0)
+        return res
+        
+### BFS SOLUTION 
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qlen = len(q)
+            level = []
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+
+        return res
+        
