839ec8896699c7c1bdf622415694cac2cb802d10
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-11-21 18:46:43 -0800

    solved: 347

--- solutions/347_Top-K-Freq-Elements.py
+++ solutions/347_Top-K-Freq-Elements.py
@@ -1,8 +1,5 @@
-# Given an integer array nums and an integer k, return the k most frequent elements. 
-# You may return the answer in any order.
-
-
 class Solution:
+    # O(n) time, O(n) space, bucket sort approach
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
         count = [[] for _ in range(len(nums) + 1)] 
@@ -15,3 +12,26 @@ class Solution:
             res.extend(n)
             if len(res) == k:
                 return res
+
+    # O(klogn) time, O(n) space, max heap approach
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        max_heap = [(-v, k) for k, v in freq.items()]
+        heapq.heapify(max_heap)
+        res = []
+
+        for _ in range(k):
+            res.append(heapq.heappop(max_heap)[1])
+        return res
+
+
+    # O(nlogk) time, O(n) space, min heap approach
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        heap = []
+        for key, val in freq.items():
+            heapq.heappush(heap, (val, key))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [key for _, key in heap] 
