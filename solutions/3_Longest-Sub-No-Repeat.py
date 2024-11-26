ddc5c3c3281f9f23243cc198199419688fae4cc4
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-11-25 18:29:20 -0800

    update: 3

--- solutions/3_Longest-Sub-No-Repeat.py
+++ solutions/3_Longest-Sub-No-Repeat.py
@@ -8,6 +8,19 @@ class Solution:
                 chars.remove(s[l])
                 l += 1
             chars.add(s[r])
+            res = max(res, r - l + 1)
             r += 1
-            res = max(res, r - l)
+        return res
+
+# Optimized version
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mp = {}
+        l = res = 0
+
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(l, mp[s[r]] + 1)
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
         return res
