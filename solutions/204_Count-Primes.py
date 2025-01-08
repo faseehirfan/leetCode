78dee28769a6121b49270607bc696dd9746a6803
Faseeh Irfan <faseeh_irfan@outlook.com>
2025-01-07 22:52:44 -0800

    solved: 204

---
+++ solutions/204_Count-Primes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+        is_prime = [True for _ in range(n)]
+        is_prime[0] = is_prime[1] = False
+
+        for i in range(2, math.ceil(math.sqrt(n))):
+            if is_prime[i]:
+                for j in range(i*i, n, i):
+                    is_prime[j] = False
+        
+        return sum(is_prime)
