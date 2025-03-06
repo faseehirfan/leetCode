41adfcd5ad1e020f25314a29dca50b7ea39677f7
Faseeh Irfan <faseeh_irfan@outlook.com>
2025-03-06 14:22:08 -0500

    solved: 300

---
+++ solutions/300_LIS.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range (1, n):
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i] , dp[j] + 1)
+        
+        return max(dp)
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for num in nums:
+            i = bisect_left(sub, num)
+            if i == len(sub):
+                sub.append(num)
+            else:
+                sub[i] = num
+        return len(sub)
