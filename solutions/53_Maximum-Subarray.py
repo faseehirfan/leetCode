2dc63aa96fb891b915403eefebc242076ffddb84
Faseeh Irfan <faseeh_irfan@outlook.com>
2025-04-24 03:31:17 -0400

    update: 53

--- solutions/53_Maximum-Subarray.py
+++ solutions/53_Maximum-Subarray.py
@@ -7,3 +7,38 @@ class Solution:
             curSum += n
             res = max(res, curSum)
         return res
+    
+
+## dp solution
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        def maxSubRecursive(l, r):
+            if l == r:
+                return nums[l]
+            
+            mid = l + (r-l) // 2
+            left_sum = maxSubRecursive(0, mid)
+            right_sum = maxSubRecursive(mid+1,r)
+            cross_sum = 0
+
+            count = 0
+            max_left = float('-inf')
+
+            for i in range(mid, l - 1, -1):
+                count += nums[i]
+                max_left = max(count, max_left)
+            
+            count = 0
+            max_right = float('-inf')
+
+            for i in range(mid + 1, r + 1):
+                count += nums[i]
+                max_right = max(count, max_right)
+            
+            cross_sum = max_left + max_right
+
+            return max(left_sum, right_sum, cross_sum)
+
+
+        return maxSubRecursive(0, len(nums)-1)
