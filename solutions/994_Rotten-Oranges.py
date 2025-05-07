4580a0ffe16813f063b743fa7c3421ca0b56731e
Faseeh Irfan <faseeh_irfan@outlook.com>
2025-05-06 23:31:44 -0700

    solved: 994

---
+++ solutions/994_Rotten-Oranges.py
@@ -0,0 +1,32 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        time = -1
+        q = deque()
+        visited = set()
+        fresh = 0
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+        
+  
+
+        while q:
+            for _ in range(len(q)):
+                row, col = q.popleft()
+                for dr, dc in [[1,0], [-1,0], [0,1], [0,-1]]:
+                    nr, nc = row + dr, col + dc
+                    if (0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1 and (nr, nc) not in visited):
+                        visited.add((nr,nc))
+                        q.append((nr,nc))
+                        fresh -= 1
+            time += 1
+
+        if fresh > 0:
+            return -1
+        return max(time, 0)
