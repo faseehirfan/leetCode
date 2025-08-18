51e0c95a020fa4de62d7143c92fa1d594e78068f
Faseeh Irfan <faseeh_irfan@outlook.com>
2025-08-17 17:28:24 -0700

    solved: 72

---
+++ solutions/72_Edit-Distance.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        if not word1 or not word2:
+            return max(len(word1), len(word2))
+        
+        if word1 == word2: return 0
+
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+
+        for i in range(n+1):
+            dp[0][i] = i
+        
+        for i in range(m+1):
+            dp[i][0] = i
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j])
+
+        return dp[m][n]
+
+
+        
