9e256af3070eacbf03dbaee0dc38d9d08b59e865
Faseeh Irfan <faseeh_irfan@outlook.com>
2025-10-16 13:28:20 +0200

    solved: 993

---
+++ solutions/993_Cousins-Binary-Tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        q = deque([(root, None)])
+
+        while q:
+            level_size = len(q)
+            x_parent = y_parent = None
+
+            for _ in range(level_size):
+                node, parent = q.popleft()
+
+                if node.val == x:
+                    x_parent = parent
+
+                if node.val == y:
+                    y_parent = parent
+                
+                if node.left:
+                    q.append((node.left, node))
+                if node.right:
+                    q.append((node.right, node))
+                
+            if x_parent and y_parent:
+                return x_parent != y_parent
+            if x_parent or y_parent:
+                return False
+
+        return False
